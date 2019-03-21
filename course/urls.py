892f2de71926b097af6e0a9fcc2c76a3a56023ce
huangzhenwen2018 <zhenwenhuang@qq.com>
2019-03-21 16:25:46 +0800

    django laoqi

---
+++ course/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import  url
+from django.views.generic import TemplateView
+from .views import AboutView, CourseListView, ManageCourseListView, CreateCourseView, DeleteCourseView, CreateLessonView, ListLessonView, DetailLessonView, StudentListLessionView
+
+urlpatterns = [
+#    url(r'about/$', TemplateView.as_view(template_name="course/about.html")),
+     url(r"about/$", AboutView.as_view(), name="about"),
+     url(r"course-list/$", CourseListView.as_view(), name="course_list"),
+     url(r"manage-course/$", ManageCourseListView.as_view(), name="manage_course"),
+     url(r'create-course/$', CreateCourseView.as_view(), name='create_course'),
+     url(r'delete-course/(?P<pk>\d+)/$', DeleteCourseView.as_view(), name='delete-course'),
+     url(r'create-lesson/$', CreateLessonView.as_view(),name="create_lesson"),
+     url(r"list-lessons/(?P<course_id>\d+)/$",ListLessonView.as_view(),name="list_lessons"),
+     url(r"detail-lesson/(?P<lesson_id>\d+)/$",DetailLessonView.as_view(),name='detail_lesson' ),
+     url(r'lessons-list/(?P<course_id>\d+)/$', StudentListLessionView.as_view(), name='lessons-list'),
+]
---
+++ image/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from .forms import ImageForm
+from .models import Image
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# Create your views here.
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+@require_POST
+def upload_image(request):
+    form = ImageForm(data=request.POST)
+    if form.is_valid():
+        try:
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            return JsonResponse({'status':"1"})
+        except:
+            return JsonResponse({'status':"0"})
+
+@login_required(login_url='/account/login')
+def list_images(request):
+    images = Image.objects.filter(user=request.user)
+    paginator = Paginator(images, 4)
+    page = request.GET.get('page')
+    try:
+        current_page = paginator.page(page)
+        images_list = current_page.object_list
+    except PageNotAnInteger:
+        current_page = paginator.page(1)
+        images_list = current_page.object_list
+    except EmptyPage:
+        current_page = paginator.page(paginator.num_pages)
+        images_list = current_page.object_list
+
+    return render(request, 'image/list_images.html', {'images':images_list,'page': current_page })
+
+@login_required(login_url='/account/login')
+@require_POST
+@csrf_exempt
+def del_image(request):
+    image_id = request.POST['image_id']
+    try:
+        image = Image.objects.get(id=image_id)
+        image.delete()
+        return JsonResponse({'status': '1'})
+    except:
+        return JsonResponse({'status': '2'})
+
+def falls_images(request):
+    images = Image.objects.all()
+    return render(request, 'image/falls_images.html', {"images": images})
